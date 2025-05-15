d3d70bd3c823b5c20699fe986526d5b3c031dbc5
Lucky31001 <kilian.dumaire@gmail.com>
2025-05-15 21:23:06 +0200

    dev can create .jar

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,11 +6,11 @@ module org.example.thegreatests {
     requires java.desktop;
 
     opens org.example.thegreatests to javafx.fxml;
-    opens org.example.thegreatests.Views to javafx.fxml;
     opens org.example.thegreatests.Controllers to javafx.fxml, ormlite.jdbc;
     opens org.example.thegreatests.Models to ormlite.jdbc;
 
     exports org.example.thegreatests.Models;
     exports org.example.thegreatests.Views;
     exports org.example.thegreatests.Controllers;
+    opens org.example.thegreatests.Views to javafx.fxml, ormlite.jdbc;
 }
